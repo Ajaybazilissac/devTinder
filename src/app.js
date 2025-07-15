c7a5b7857aa6ead00d5fb6d2654693179e325cb8
Ajaybazilissac <ajaybazilissac93@gmail.com>
2025-07-15 22:23:09 +0530

    Get APIs

--- src/app.js
+++ src/app.js
@@ -15,6 +15,41 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// find user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+    res.send(user);
+    // const user = await User.find({ emailId: userEmail });
+    // if (user.length === 0) {
+    //   res.status(404).send("User not found");
+    // } else {
+    //   res.send(user);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("There is no user");
+    } else {
+      res.send(users);
+    }
+  } catch {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
